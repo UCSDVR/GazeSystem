7dc0c9a4d3d2d2991de37064134341eaf078e715
connor-a-smith <c3smith@ucsd.edu>
2016-02-16 20:52:09 -0800

    Added gaze statics
    - Added gaze statics, to be used as singletons
    - Contains static variables for cursor animation
    - Removed need for [HideInInspector] values

---
+++ Assets/Gaze System/Scripts/GazeStatics.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * Author: Connor Smith
+ * Version: 3.0
+ * Date: 02/16/16
+ */
+
+using UnityEngine;
+using System.Collections;
+
+/*
+ * Provides static variables for use with the gaze
+ * system. Yay singletons.
+ */
+public class GazeStatics : MonoBehaviour {
+
+  //the cursor animator object, to be dragged in
+  public Animator cursorAnimatorObj;
+
+  //the cursor animation object, to be dragged in
+  public AnimationClip cursorAnimationObj;
+
+  //static cursor Animator for use with Gaze System
+  public static Animator cursorAnimator;
+
+  //static cursor Animation for use with Gaze System
+  public static AnimationClip cursorAnimation;
+
+	// Use this for initialization
+	void Start () {
+
+    //sets static variables based on dragged values
+    cursorAnimator = cursorAnimatorObj;
+    cursorAnimation = cursorAnimationObj;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
