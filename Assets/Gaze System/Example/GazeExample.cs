8c8a9264e7f69b9d3e320b3325c0f00f585caa38
connor-a-smith <c3smith@ucsd.edu>
2016-02-16 20:51:30 -0800

    Minor updates to gaze example scene
    - Removed countdown feature, too confusing

--- Assets/Gaze System/Example/GazeExample.cs
+++ Assets/Gaze System/Example/GazeExample.cs
@@ -9,59 +9,11 @@ using System.Collections;
 
 public class GazeExample : MonoBehaviour, Gazeable {
 
-  //Text Mesh component for the countdown example
-  public TextMesh textTimer;
-
-  //duration of countdown timer
-  public int timerDuration;
-
-  //in what interval the timer should count down
-  public int timerInterval;
-
-  //current value of the timer
-  private int currentTimer;
-
-  //string for the timer
-  private string timerString = "You May Activate In ";
-
-  //recorded game time for countdown timer
-  private float lastRecordedTime;
-
 	// Use this for initialization
-	void Start () {
-
-    //Stores the initial game time
-	  lastRecordedTime = Time.time;
-
-    //initializes the current timer value
-	  currentTimer = timerDuration;
-
-    //Updates the timer text
-    textTimer.text = timerString + timerDuration;
-	
-	}
+	void Start () { }
 	
 	// Update is called once per frame
-	void Update () {
-
-    //checks if there's any time left on the timer
-	  if (currentTimer > 0) {
-
-      //checks if the time interval has passed
-	    if (Time.time - lastRecordedTime > timerInterval) {
-
-        //decrements the timer
-	      currentTimer--;
-
-        //updates timer text
-	      textTimer.text = timerString + currentTimer.ToString();
-
-        //resets recorded time to current time
-	      lastRecordedTime = Time.time;
-
-	    }
-	  }
-	}
+	void Update () { }
 
   /*
    * Required method for use with the Gaze system, based on
@@ -72,15 +24,8 @@ public class GazeExample : MonoBehaviour, Gazeable {
    */
 	public bool CheckGazeConditions() {
 
-    //if the timer is at zero, gaze should activate
-	  if (currentTimer == 0) {
-
-	    return true;
-
-	  }
-
-    //if the timer isn't at zero, gaze shouldn't activate
-	  return false;
+    //always returns true, gaze conditions always met
+    return true;
 
 	}
 
@@ -98,27 +43,19 @@ public class GazeExample : MonoBehaviour, Gazeable {
     //randomizes object color
 	  RandomizeColor();
 
-    //resets the timer to start again
-	  ResetTimer();
-
 	}
 
   /*
-   * Starts timer at beginning again
+   * Required method for use with the Gaze System, based on
+   * implementation of Gazeable. This method will run when
+   * the object has already been activated, and the user then
+   * looks away. 
    */
-	void ResetTimer() {
+  public void Deactivate() { 
 
-    //sets timer back to beginning
-	  currentTimer = timerDuration;
 
-    //updates text on the timer
-    textTimer.text = timerString + timerDuration.ToString();
-
-    //updates last recorded time to current time
-    lastRecordedTime = Time.time;
-
-	}
-
+  }
+ 
   /*
    * Sets a random color on this object
    */
